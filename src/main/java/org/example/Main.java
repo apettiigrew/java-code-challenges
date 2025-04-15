994ce0c0d820b3df6af99013d4079f802bfa92be
Andrew Pettigrew <devdrew10@gmail.com>
2025-04-15 15:52:47 -0400

    add isValid challenge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,15 @@
 package org.example;
 
-import arrays.ArraySolution;
-
 import java.util.*;
-
-import static java.util.stream.Collectors.toList;
 
 public class Main {
     public static void main(String[] args) {
-        MinStack stack = new MinStack();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack.printStack());
-        stack.pop();
-        System.out.println(stack.printStack());
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()){
+            int num = scanner.nextInt();
+            System.out.println(num);
+        }
 
     }
 
@@ -31,22 +26,9 @@ public class Main {
         return false;
     }
 
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (var i = 0; i < nums.length; i++) {
-            Integer index = map.get(nums[i]);
-            if (index != null) {
-                return new int[]{i, index};
-            }
-            map.put(target - nums[i], i);
-        }
-
-        return nums;
-    }
-
     public static boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length())
+        if (s.length() != t.length())
             return false;
 
         Map<Character, Integer> sMap = new HashMap<>();
@@ -63,12 +45,12 @@ public class Main {
         Map<Character, Integer> tMap = new HashMap<>();
         for (var j = 0; j < t.length(); j++) {
             char tLetter = t.charAt(j);
-            if(sMap.containsKey(tLetter)){
+            if (sMap.containsKey(tLetter)) {
                 Integer sCount = sMap.get(tLetter);
                 Integer tCount = tMap.get(tLetter);
 
-                if(tCount == null || tCount <= sCount){
-                    if(tMap.containsKey(tLetter)){
+                if (tCount == null || tCount <= sCount) {
+                    if (tMap.containsKey(tLetter)) {
                         Integer occurrence = tMap.get(tLetter);
                         tMap.put(tLetter, occurrence + 1);
                     } else {
@@ -84,11 +66,11 @@ public class Main {
         }
 
         // loop over each Map to get the occurrence of each letter
-        for(Map.Entry<Character, Integer> entry: sMap.entrySet()){
+        for (Map.Entry<Character, Integer> entry : sMap.entrySet()) {
             Character letter = entry.getKey();
             Integer sCount = entry.getValue();
             Integer tcount = tMap.get(letter);
-            if(!sCount.equals(tcount)){
+            if (!sCount.equals(tcount)) {
                 return false;
             }
         }
@@ -96,29 +78,62 @@ public class Main {
         return true;
     }
 
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (var i = 0; i < s.length(); i++) {
+            var c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else  if(stack.isEmpty()) {
+                    return false;
+                } else if (c == ')' && stack.peek() == '('){
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (var i = 0; i < nums.length; i++) {
+            Integer index = map.get(nums[i]);
+            if (index != null) {
+                return new int[]{i, index};
+            }
+            map.put(target - nums[i], i);
+        }
+
+        return nums;
+    }
+
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> freqMap = new HashMap<>();
-        for(var num: nums){
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (var num : nums) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer>[] buckets = new List[nums.length+1];
+        List<Integer>[] buckets = new List[nums.length + 1];
 
-        freqMap.forEach((num,freq) -> {
-            if(buckets[freq] == null) {
+        freqMap.forEach((num, freq) -> {
+            if (buckets[freq] == null) {
                 buckets[freq] = new ArrayList<>();
                 buckets[freq].add(num);
             }
         });
 
         List<Integer> result = new ArrayList<>();
-        for(int i = buckets.length - 1; i > 0 && result.size() < k;i--){
-            if(buckets[i] != null){
+        for (int i = buckets.length - 1; i > 0 && result.size() < k; i--) {
+            if (buckets[i] != null) {
                 result.addAll(buckets[i]);
             }
         }
 
         return result.stream().mapToInt(i -> i).toArray();
     }
-
 }
